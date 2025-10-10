f2ab05659ecc2b5e29dc35b73d2bd7b76f62f741
upperbrandon <108894706+upperbrandon@users.noreply.github.com>
2025-10-09 23:59:03 -0600

    Got first Preds

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -1,3 +1,4 @@
+# Load Libraries ----------------------------------------------------------
 library(tidyverse)
 library(tidymodels)
 library(vroom)
@@ -7,43 +8,42 @@ library(patchwork)
 library(glmnet)
 library(ranger)
 library(ggmosaic)
-library(embed) 
-# Read and Set ------------------------------------------------------------
+library(embed)
 
+# Read and Set ------------------------------------------------------------
 setwd("~/GitHub/AmazonEmployeeAccess")
 train_data <- vroom("train.csv")
-test_data <- vroom("test.csv")
+test_data  <- vroom("test.csv")
 
+# Ensure ACTION is a factor -----------------------------------------------
+train_data <- train_data %>%
+  mutate(ACTION = factor(ACTION))
 
-# Boxplot -----------------------------------------------------------------
-
-
+# Exploratory: Mosaic Plots -----------------------------------------------
 train_data2 <- train_data %>%
   mutate(
     ROLE_ROLLUP_1 = as.factor(ROLE_ROLLUP_1),
-    ACTION = as.factor(ACTION)
+    ROLE_ROLLUP_2 = as.factor(ROLE_ROLLUP_2)
   )
 
-ggplot(data = train_data2) +
+ggplot(train_data2) +
   geom_mosaic(aes(weight = 1, x = product(ROLE_ROLLUP_1), fill = ACTION))
 
-ggplot(data = train_data2) +
+ggplot(train_data2) +
   geom_mosaic(aes(weight = 1, x = product(ROLE_ROLLUP_2), fill = ACTION))
 
-# Bake --------------------------------------------------------------------
-
-
+# Recipe ------------------------------------------------------------------
 my_recipe <- recipe(ACTION ~ ., data = train_data) %>%
-  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_mutate_at(all_numeric_predictors(), fn = as.factor) %>%
   step_other(all_nominal_predictors(), threshold = 0.001) %>%
   step_dummy(all_nominal_predictors())
 
 
-# Apply the recipe
+# Prep and Bake -----------------------------------------------------------
+Prepped <- prep(my_recipe)
+baked_train <- bake(Prepped, new_data = train_data)
 
-prep <- prep(my_recipe)
-baked <- bake(prep, new_data = train_data)
-
+# Model -------------------------------------------------------------------
 logRegModel <- logistic_reg() %>%
   set_engine("glm")
 
@@ -52,7 +52,16 @@ logReg_workflow <- workflow() %>%
   add_model(logRegModel) %>%
   fit(data = train_data)
 
+# Predict -----------------------------------------------------------------
 amazon_predictions <- predict(logReg_workflow,
-                              new_data=test_data,
-                              type=prob)
+                              new_data = test_data,
+                              type = "prob") %>%
+  select(.pred_1) %>%
+  rename(ACTION = .pred_1)
 
+# Create submission -------------------------------------------------------
+kaggle_submission <- test_data %>%
+  select(id) %>%
+  bind_cols(amazon_predictions)
+
+vroom_write(kaggle_submission, file = "./LinearPreds.csv", delim = ",")
